3904e3b4cb1fe9188d35ab6f8d36f04eb6a57521
dreadwing5 <sachinspyk@outlook.com>
2026-06-21 00:26:45 +0530

    feat: add Duende identity server

---
+++ src/Carsites.IdentityService/Pages/Account/AccessDenied.cshtml.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Carsites.IdentityService.Pages.Account;
+
+public class AccessDeniedModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
